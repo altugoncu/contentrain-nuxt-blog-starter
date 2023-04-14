87a6d808ec858788e25333fc05e1e293d8ca2ed1
Ekin Gün Öncü <oekingun@gmail.com>
2023-04-14 23:33:58 +0000

    players

--- components/Players.vue
+++ components/Players.vue
@@ -1,14 +1,14 @@
 
 <template>
-    <div class="flex w-full justify-around" style="height: 400px;padding: 15px">
-        <div style="height: 100%;">
-            <iframe src="https://open.spotify.com/embed/playlist/2SkvRDyiF5Grj5kQhaHm6o" style="width: 100%;height: 100%"
+    <div style="height: 500px;" class="flex flex-col md:flex-row w-full justify-center items-center">
+        <div class="md:col-span-1 md:pr-5 md:pl-5 lg:p-10 lg:w-1/2 md:h-1/2 h-full flex justify-center items-center">
+            <iframe src="https://open.spotify.com/embed/playlist/2SkvRDyiF5Grj5kQhaHm6o" class="w-full" height="100%"
                 frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         </div>
-        <div style="height: 100%;">
-            <iframe flex w-full justify-around style="border: 0; width: 100%; height: 100%;"
+        <div class="md:col-span-3 md:pr-5 md:pl-5 lg:p-10 lg:w-1/2 md:h-1/2 h-full md:h-auto flex justify-center items-center">
+            <iframe class="md:h-1/2 h-full"
                 src="https://bandcamp.com/EmbeddedPlayer/track=429637163/size=large/bgcol=333333/linkcol=e99708/minimal=true/transparent=true/"
-                seamless><a href="https://meshkmusic.bandcamp.com/track/metamorphosis">Metamorphosis by Meshk</a></iframe>
+                frameborder="0" allowtransparency="true"></iframe>
         </div>
     </div>
 </template>
